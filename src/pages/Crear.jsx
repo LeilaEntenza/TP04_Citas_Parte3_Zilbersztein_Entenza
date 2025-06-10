37ad2e17af2909a711eca859df08ecb4625cd26b
Android Studio <devandroid@ort>
2025-06-10 09:18:20 -0300

    Modificaciones diseño

--- src/pages/Crear.jsx
+++ src/pages/Crear.jsx
@@ -6,7 +6,6 @@ import { Form } from '../Form'
 export default function Crear({ lista, setLista }) {
   return (
     <>
-      <h1>Fijar una cita</h1>
       <div className='formC'>
         <div className='form'>
         <Form listado={lista} setLista={setLista}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Home.css';
 
 export default function Home() {
   return (
     <div><h1>Citas</h1>
-      <p>Concretá citas para que atendamos a tus mascotas!</p>
+      <p className='textHome'>Concretá citas para que atendamos a tus mascotas!</p>
       <div className='container'>
-        <img width="400" src='https://i.pinimg.com/736x/10/bc/bd/10bcbdc51fdacda178fbf70267e19251.jpg'/>
+        <img width="350" src='https://i.pinimg.com/originals/5f/6f/0c/5f6f0cc5877d1076d3eccdc4b0d5964d.gif'/>
   </div></div>
   )
 }
